0a2b33290d2cdf2ae2b5db3f7f581629c039bf35
iclembot <iclembot@gmail.com>
2023-10-22 03:03:07 -0300

    Got deps built by building bevy0.11.3 first using gnu chain , then switching to msvc and building project with properties->project-references->bevy checked

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ use worldgen::noisemap::{NoiseMapGenerator, NoiseMap, Seed, Step, Size};
 use worldgen::world::{World, Tile};
 use worldgen::world::tile::{Constraint, ConstraintType};
 use rand_seed::rand_seed;
-
+use bevy_tilemap::prelude::*;
+use bevy::asset::HandleId;
+use bevy::prelude::*;
 
 fn main() {
     let noise = PerlinNoise::new();
@@ -17,7 +19,7 @@ fn main() {
         .set(Step::of(0.005, 0.005));
 
     let nm2 = NoiseMap::new(noise)
-        .set(Seed::of("Hello!"))
+        .set(Seed::of(rand_seed(6)))
         .set(Step::of(0.05, 0.05));
 
     let nm = Box::new(nm1 + nm2 * 3);
@@ -51,4 +53,34 @@ fn main() {
 
         println!("");
     }
+}
+
+fn tilemap_now() {
+    
+    // Build a default Tilemap with 32x32 pixel tiles.
+    let mut tilemap = Tilemap::default();
+    
+    // We need a Asset<TextureAtlas>. For this example we get a random one as a placeholder.
+    let texture_atlas_handle = Handle::weak(HandleId::random::<TextureAtlas>());
+    
+    // Set the texture atlas for the Tilemap
+    tilemap.set_texture_atlas(texture_atlas_handle);
+    
+    // Create tile data
+    let tile = Tile {
+        // 2D location x,y (units are in tiles)
+        point: (16,16),
+        
+        // Which tile from the TextureAtlas
+        sprite_index: 0,
+        
+        // Which z-layer in the Tilemap (0-up)
+        sprite_order: 0,
+        
+        // Give the tile an optional green tint
+        tint: bevy::render::color::Color::GREEN,
+        };
+    
+    // Insert a single tile
+    tilemap.insert_tile( tile);
 }
