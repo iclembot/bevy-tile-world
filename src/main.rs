2b3059f34e57750d98cec67f26c7e8c429d69839
iclembot <iclembot@gmail.com>
2023-10-22 22:54:21 -0300

    Switched to bevy-ecs-tilemap, build ok, not tested yet

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use worldgen::noisemap::{NoiseMapGenerator, NoiseMap, Seed, Step, Size};
 use worldgen::world::{World, Tile};
 use worldgen::world::tile::{Constraint, ConstraintType};
 use rand_seed::rand_seed;
-use bevy_tilemap::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
 use bevy::asset::HandleId;
 use bevy::prelude::*;
 
@@ -56,31 +56,4 @@ fn main() {
 }
 
 fn tilemap_now() {
-    
-    // Build a default Tilemap with 32x32 pixel tiles.
-    let mut tilemap = Tilemap::default();
-    
-    // We need a Asset<TextureAtlas>. For this example we get a random one as a placeholder.
-    let texture_atlas_handle = Handle::weak(HandleId::random::<TextureAtlas>());
-    
-    // Set the texture atlas for the Tilemap
-    tilemap.set_texture_atlas(texture_atlas_handle);
-    
-    // Create tile data
-    let tile = Tile {
-        // 2D location x,y (units are in tiles)
-        point: (16,16),
-        
-        // Which tile from the TextureAtlas
-        sprite_index: 0,
-        
-        // Which z-layer in the Tilemap (0-up)
-        sprite_order: 0,
-        
-        // Give the tile an optional green tint
-        tint: bevy::render::color::Color::GREEN,
-        };
-    
-    // Insert a single tile
-    tilemap.insert_tile( tile);
 }
